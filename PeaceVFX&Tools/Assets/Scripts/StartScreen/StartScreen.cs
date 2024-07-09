2e4e3366f1a2813574dff9c8d543a12e2638c25f
Narai Risser (RIT Student) <43816475+Tonsak09@users.noreply.github.com>
2024-07-09 17:01:00 -0400

    Start screen

---
+++ PeaceVFX&Tools/Assets/Scripts/StartScreen/StartScreen.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreen : MonoBehaviour
+{
+    /// <summary>
+    /// Continue to the most recent save file 
+    /// </summary>
+    public void Continue()
+    {
+        // Load using FileIO system 
+
+        // Load correct scene 
+    }
+
+    /// <summary>
+    /// Reload the current save file to default values 
+    /// </summary>
+    public void NewGame()
+    {
+        // Override current file 
+    }
+
+    /// <summary>
+    /// Access the settings to the game 
+    /// </summary>
+    public void Settings()
+    {
+
+    }
+
+    /// <summary>
+    /// Closes the game 
+    /// </summary>
+    public void ExitGame()
+    {
+
+    }
+}
